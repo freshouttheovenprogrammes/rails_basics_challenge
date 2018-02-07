464f5f6d6ee1274becd1953738dec6deaf7b1909
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-07 14:58:21 -0700

    adds edit link test

--- spec/features/user_can_edit_a_current_student_spec.rb
+++ spec/features/user_can_edit_a_current_student_spec.rb
@@ -3,11 +3,21 @@ require 'rails_helper'
 describe "As a user" do
   context "I can edit a student" do
     it "by going to the student edit page" do
-      student_1 = Student.create!(first_name: "Gergory", last_name: "Phulls")
+      Student.create!(first_name: "Gergory", last_name: "Phulls")
 
       visit students_path
 
       expect(page).to have_link 'Edit'
+    end
+
+    it "when I click edit link on student/index" do
+      student = Student.create!(first_name: "Gergory", last_name: "Phulls")
+
+      visit students_path
+
+      click_on('Edit')
+
+      expect(current_path).to eq "students/#{student.id}/edit"
     end
   end
 end
