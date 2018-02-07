5471d83a07feb33d789a8e9f0b8f080ad098edd3
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-07 13:51:55 -0700

    fixed db woohoo

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,10 @@ ActiveRecord::Schema.define(version: 20180207202810) do
   enable_extension "plpgsql"
 
   create_table "students", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
