67d7c777d74c2f8616f2029880add9b2e8ce50b5
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-07 15:36:27 -0700

    all navigation tests complete

--- spec/features/user_can_nav_to_index_from_any_path_spec.rb
+++ spec/features/user_can_nav_to_index_from_any_path_spec.rb
@@ -1,14 +1,73 @@
 require 'rails_helper'
 
 describe "As a user" do
-  it "when I'm on the students show I can get back to index" do
-    student = Student.create!(first_name: "Zac", last_name: "Phullmquist")
+  context "testing index links" do
+    it "when I'm on the students show path I can get back to index" do
+      student = Student.create!(first_name: "Zac", last_name: "Phullmquist")
 
-    visit student_path(student)
+      visit student_path(student)
 
-    expect(page).to have_link('Index')
-    click_on('Index')
+      expect(page).to have_link('Index')
+      click_on('Index')
 
-    expect(current_path).to eq '/students'
+      expect(current_path).to eq '/students'
+    end
+
+    it "when I'm on the students edit path I see both create students and all students link" do
+      student = Student.create!(first_name: "Zac", last_name: "Phullmquist")
+
+      visit edit_student_path(student)
+
+      expect(page).to have_link('Index')
+      expect(page).to have_link('Create New Student')
+
+      click_on('Index')
+
+      expect(current_path).to eq '/students'
+    end
+
+    it "when I'm on the students delete path I can get back to index" do
+      student = Student.create!(first_name: "Zac", last_name: "Phullmquist")
+
+      visit edit_student_path(student)
+
+      expect(page).to have_link('Index')
+      click_on('Index')
+
+      expect(current_path).to eq '/students'
+    end
   end
+  context "all paths have create student link and take you there" do
+      it "when I'm on the students index path I can create a new student" do
+        visit students_path
+
+        expect(page).to have_link('Create New Student')
+
+        click_on ('Create New Student')
+
+        expect(current_path).to eq '/students/new'
+      end
+
+      it "when I'm on the students edit path I can create a new student" do
+        student = Student.create!(first_name: "Eva", last_name: "Toon")
+
+        visit student_path(student)
+
+        expect(page).to have_link('Create New Student')
+
+        click_on ('Create New Student')
+
+        expect(current_path).to eq '/students/new'
+      end
+
+      it "when I'm on the students index path I can create a new student" do
+        visit students_path
+
+        expect(page).to have_link('Create New Student')
+
+        click_on ('Create New Student')
+
+        expect(current_path).to eq '/students/new'
+      end
+    end
 end
