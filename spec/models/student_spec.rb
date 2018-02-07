b6bcd67bd594d2649383400f51707cd18afeb88c
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-07 14:14:58 -0700

    model tests passing

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,10 +2,22 @@ require 'rails_helper'
 
 describe Student do
   context "Validations" do
-    it "student is invalid without a name" do
-      student = Student.create!(first_name: nil)
+    it "student is invalid without a first name" do
+      student = Student.create(last_name: "Johnson")
 
       expect(student).to_not be_valid
+    end
+
+    it "student is invalid without a last name" do
+      student = Student.create(first_name: "Johnson")
+
+      expect(student).to_not be_valid
+    end
+
+    it "student is valid with all the names" do
+      student = Student.create(first_name: "Johnson", last_name: "Totta")
+
+      expect(student).to be_valid
     end
   end
 end
